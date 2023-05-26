0cdef16297c2ee474576d1765ea273201f82da74
Moses <lazaromoses77@gmail.com>
2023-05-26 18:57:00 +0300

    Adding server url

--- src/axios-client.jsx
+++ src/axios-client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://lazy-puce-fossa-belt.cyclic.app/api"
 })
 
 axiosClient.interceptors.request.use((config) => {
